0e74054c46d7ebb2a41dfccaac155429fdd0b586
Hussein Abdallah <sain.abdallah@gmail.com>
2023-04-16 17:08:26 -0400

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5050;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
