9c216db45b54d3ceff566f4f771431bb920cd014
Hussein Abdallah <sain.abdallah@gmail.com>
2023-05-02 18:17:43 -0400

    Setup project routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import {AuthPage, IndexPage, NotFoundPage, RootLayout} from './components';
+
 export function App() {
-  return <div className="App">Bucket List App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        path="/"
+        element={<RootLayout />}
+        ErrorBoundary={(error) => <div>Error: {error.message}</div>}
+      >
+        <Route index path="/" element={<IndexPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>,
+    ),
+  );
+
+  return <RouterProvider router={router} />;
 }
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export {RootLayout} from './RootLayout';
+export {IndexPage} from './IndexPage';
+export {AuthPage} from './AuthPage';
+export {NotFoundPage} from './NotFoundPage';
