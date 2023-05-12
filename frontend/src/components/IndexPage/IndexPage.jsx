9f09cc9713a1fa93ee91f4b2c775b0b14d28551f
Hussein Abdallah <sain.abdallah@gmail.com>
2023-05-11 23:13:05 -0400

    Create Header component

--- frontend/src/components/IndexPage/IndexPage.jsx
+++ frontend/src/components/IndexPage/IndexPage.jsx
@@ -1,3 +1,10 @@
+import {Header} from '../Header';
+import styles from './IndexPage.module.css';
+
 export function IndexPage() {
-  return <div>IndexPage</div>;
+  return (
+    <div className={styles.Container}>
+      <Header />
+    </div>
+  );
 }
